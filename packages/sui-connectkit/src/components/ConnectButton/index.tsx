8fe014c9e61193a756fa385ae23249e7ef62bcad
lybenson <gwanbit@gmail.com>
2024-05-07 22:13:08 +0800

    chore: typescript compilation configuration

--- packages/sui-connectkit/src/components/ConnectButton/index.tsx
+++ packages/sui-connectkit/src/components/ConnectButton/index.tsx
@@ -1,1 +1,17 @@
-export default function ConnectButton() {}
+import React, { useContext, useEffect, useState } from 'react'
+
+export interface ConnectButtonProps {
+  label?: string
+  showBalance?: boolean
+  showAvatar?: boolean
+  mode: 'light' | 'dark'
+}
+
+export function ConnectButton({
+  label = 'Connect',
+  showBalance = true,
+  showAvatar = true,
+  mode = 'light'
+}) {
+  return <div>{label}</div>
+}
