7b8cbc48f4a8359d53d65938dfdfaf2bcb4e52cd
lybenson <gwanbit@gmail.com>
2024-05-23 23:46:06 +0800

    feat: sign message

---
+++ packages/smartkit-hooks/src/hooks/useSignMessage.ts
@@ -0,0 +1,58 @@
+import { useMutation } from '@tanstack/react-query'
+import { useConnectStore } from './useConnectStore'
+import { UseMutationParameters } from '../types/utils'
+import { SuiSignPersonalMessageOutput } from '@mysten/wallet-standard'
+
+type SignMessageParameters = {
+  message: string
+}
+
+type SignMessageReturn = SuiSignPersonalMessageOutput
+
+type UseSignMessageParameters = {
+  mutation?: UseMutationParameters<
+    SignMessageReturn | undefined,
+    Error,
+    SignMessageParameters
+  >
+}
+
+export function useSignMessage(parameters: UseSignMessageParameters = {}) {
+  const { mutation } = parameters
+  const currentAccount = useConnectStore((state) => state.currentAccount)
+  const currentWallet = useConnectStore((state) => state.currentWallet)
+
+  const { mutateAsync, ...result } = useMutation({
+    ...mutation,
+    mutationKey: ['signMessage'],
+    mutationFn: async ({ message }) => {
+      if (!currentAccount || !currentWallet) {
+        throw new Error('No account is connected.')
+      }
+
+      const signPersonalMessageFeature =
+        currentWallet.features['sui:signPersonalMessage']
+      if (signPersonalMessageFeature) {
+        return await signPersonalMessageFeature.signPersonalMessage({
+          message: new TextEncoder().encode(message),
+          account: currentAccount
+        })
+      }
+      const signMessageFeature = currentWallet.features['sui:signMessage']
+      if (signMessageFeature) {
+        const { messageBytes: bytes, signature } =
+          await signMessageFeature.signMessage({
+            message: new TextEncoder().encode(message),
+            account: currentAccount
+          })
+
+        return { bytes, signature }
+      }
+
+      throw new Error(
+        `${currentWallet.name} does not support signing messages.`
+      )
+    }
+  })
+  return { ...result, signMessage: mutateAsync }
+}
