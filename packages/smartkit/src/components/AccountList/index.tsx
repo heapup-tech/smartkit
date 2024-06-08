928fb8c636ff888741fcb9ad2d7167973e65afc3
lybenson <gwanbit@gmail.com>
2024-06-08 19:03:11 +0800

    feat: dropdown

--- packages/smartkit/src/components/AccountList/index.tsx
+++ packages/smartkit/src/components/AccountList/index.tsx
@@ -1,13 +1,21 @@
 import { useAccount } from '@heapup/smartkit-hooks'
 import styles from './styles.css'
 import SwitchIcon from '../../icons/SwitchIcon'
+import Dropdown from '../Dropdown/Dropdown'
+import DropdownTrigger from '../Dropdown/DropdownTrigger'
 
 export default function AccountList() {
   const { accounts } = useAccount()
 
   return (
-    <div className={styles.indicator}>
-      <SwitchIcon />
+    <div>
+      <Dropdown>
+        <DropdownTrigger>
+          <div className={styles.indicator}>
+            <SwitchIcon />
+          </div>
+        </DropdownTrigger>
+      </Dropdown>
     </div>
   )
 }
---
+++ packages/smartkit/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from 'react'
+
+export default function Dropdown({ children }: PropsWithChildren) {
+  return <div>{children}</div>
+}
